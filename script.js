73913b85b3ca085729618e23b4f222ed501790b6
José Fernando <86847325+Kyuubi-09@users.noreply.github.com>
2022-10-29 02:09:05 -0300

    maduda

---
+++ script.js
@@ -0,0 +1,51 @@
+const tela = document.getElementById('frase')
+function briga(){
+        var bri=['Sei que eu devo ter feito alguma merda, por isso to tentando te fazer feliz com esse app, eu te amo meu bem e nossa faze vai passar eu prometo<3',
+        'sinceramente eu sei que deve ta doendo muito em nós, posso ta estressado agora, mas prometo entender seu lado, eu amo você e nao me leva a serio ta? eu sou bem complicado as vezes']
+
+        var list=bri[Math.floor(Math.random() * bri.length)]    
+        tela.innerHTML=list
+        
+}
+function ciume(){
+    var ciu=['olha bebe, ela pode ser na sua cabeça, a garota mais incrivel do mundo, mas pra mim nunca chegará perto de você',
+                'te trocar por isso ai? é trocar um diamante por uma pedra qualquer']
+    var list=ciu[Math.floor(Math.random() * ciu.length)]    
+    tela.innerHTML=list
+}
+function inseg(){
+        var inse=['meu amor, se for pelo seu corpo eu colocaria ele inteiro na minha boca, pena que nao cabe',
+        'seu carater e espirito são incriveis e eu to super apaixonado por você as 01:02 da manha, acha mesmo que deve se sentir insegura? kkk te amo nenem']
+    var list=inse[Math.floor(Math.random() * inse.length)]    
+    tela.innerHTML=list
+}
+function future(){
+        var fut=['sei que nosso futuro é assustador, as vezes vamos sofrer mesmo e chorar, mas saiba que eu sinto e sempre vou ter orgulho de você, e lembra o que eu disse? eu vou te dar o mundo!',
+        'qualquer futuro é perfeito se for ao seu lado amorzinho<3']
+    var list=fut[Math.floor(Math.random() * fut.length)]    
+    tela.innerHTML=list
+}
+function ansie(){
+        var ansie=['aiai a boa e velha ansiedade, é tão complciada que nem sei o que colocar aqui, mas sei que você não a merece, por que você é pura bondade, e puro amor, eu vejo em você uma beleza interior aconchegante, é simples, é carinhosa, é preocupada, aaaa você é a garota mais incrivel que ja pisou na face da terra, por favor me liga, ansiedade é complicado',
+        'nenem você precisa me ligar agora, por favooor<3']
+    var list=ansie[Math.floor(Math.random() * ansie.length)]    
+    tela.innerHTML=list
+}
+function medo(){
+        var md=['nenem com medo? de que bebeee, se for de me perder, leia o quadro da insegurança e ciume, se for sobre o futuro, tem um quadrin desse também, mas se não for isso, eu quero dizer que você nao precisa ter medo, você é capaz de enfrentar tudo isso e eu sei que é, eu fico muito grato e orgulhoso de ter você comigo, então vammoos mulher reageeee',
+                'meu amor, eu te amo tanto que qualquer coisa que te aflingir eu mato, ate esse medo ai, pode dize o que passa no pv sim? <3']
+    var list=md[Math.floor(Math.random() * md.length)]    
+    tela.innerHTML=list
+}
+function desani(){
+        var desa=['serio, eu fico com muita dó de te ver assim, espero que eu esteja te ajudando agora, e também que não seja eu o motivo, se a gente tiver brigado, me manda uma mensagem mesmo me odiando agora, eu juro que isso resolve 90% das coisas',
+        'caso seja desanimo normal, meu amor eu vou te animar agora, quem é ogudugudamaindele? brincadeira bebe, desanimo também é serio fala com nosso suporter pelo reclame aqui +55(7398180-1116)']
+    var list=desa[Math.floor(Math.random() * desa.length)]    
+    tela.innerHTML=list   
+}
+function fal_es(){
+        var fe=['o nenem, ta sem esperanças, olha eu sei que o futuro é uma escuridao enorme, e sei que é uma frase paia, mas basta ter fé, nós dois juntos vamos conseguir chegar la, tenha calma, tenha fé, eu e você vamos vencer tudo isso, eu te prometo',
+        'saiba que mesmo que um dia a gente termine, eu vou estar aqui com você sempre, te ajudando no que eu puder, te amo nenem fica bem <3']
+    var list=fe[Math.floor(Math.random() * fe.length)]    
+    tela.innerHTML=list
+}
